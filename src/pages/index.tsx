b8de6698a872cc069e63e661d34880e6040cc648
Jarad DeLorenzo <jaradd@gmail.com>
2022-09-30 20:30:15 -0400

    Added branding test. And stuff

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -16,7 +16,7 @@ function App() {
 
   return (
     <div className="container">
-      <h1>Welcome to Tauri!</h1>
+      <h1>Welcome to GameJangler!</h1>
 
       <div className="row">
         <span className="logos">
