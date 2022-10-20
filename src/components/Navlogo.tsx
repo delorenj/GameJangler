3acd68cf69a81b5a19a71855be1aa7f118d6f651
Jarad DeLorenzo <jaradd@gmail.com>
2022-10-19 22:02:49 -0400

    Trying to layout the navbar with flex

---
+++ src/components/Navlogo.tsx
@@ -0,0 +1,14 @@
+export const Navlogo = () => {
+  return (
+    <a href="#" className="flex items-center">
+      <img
+        src="https://flowbite.com/docs/images/logo.svg"
+        className="mr-3 h-6 sm:h-9"
+        alt="Game Jangler Logo"
+      />
+      <span className="self-center whitespace-nowrap text-xl font-semibold text-blue-500">
+        Game Jangler
+      </span>
+    </a>
+  )
+}
