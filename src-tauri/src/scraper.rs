600a82aa3326add87eb07b32ff99e392f9af98ac
Jarad DeLorenzo <jaradd@gmail.com>
2022-10-25 22:05:25 -0400

    Added some lib files for testing

---
+++ src-tauri/src/scraper.rs
@@ -0,0 +1,18 @@
+struct Game {
+    title: String,
+    location: String,
+    platform: Platform
+}
+
+struct Platform {
+    id: String, //UUID
+    name: String,
+    location: String
+}
+
+impl Game {
+    fn new(&mut self) {
+        self.title = `"No Title";
+
+    }
+}
