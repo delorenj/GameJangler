3d5777a38539c1c0ccd4c99a060810f7b8ee3b0c
Jarad DeLorenzo <jaradd@gmail.com>
2022-11-09 22:31:19 -0500

    Added scraper bin and can now pass options.  cargo run --bin scraper Steam -r C:/tom

--- src-tauri/src/scraper/steam/mod.rs
+++ src-tauri/src/scraper/steam/mod.rs
@@ -8,8 +8,7 @@ pub struct Steam;
 impl Scrapable<PlatformInstance> for Steam {
     fn start_scrape(&self, result: &mut Vec<PlatformInstance>, drive_letter: char) {
         println!("Scanning for Steam platforms on drive letter {}", drive_letter);
-        let test = PlatformInstance::new("Test".to_owned(), "C:/some/location".to_owned());
-        result.push(test);
+
     }
 }
 
