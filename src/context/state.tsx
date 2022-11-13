2045d38490dc77fcafc8c66d92afc40c2631b782
Jarad DeLorenzo <jdelorenzo@justworks.com>
2022-11-12 21:03:26 -0500

    Added some app state vars to global context to store app, platform, game, and server states

--- src/context/state.tsx
+++ src/context/state.tsx
@@ -1,6 +1,6 @@
-import { createContext, useContext, useState } from "react";
+import { createContext, useContext, useEffect, useState } from "react"
 
-const GlobalContext = createContext({});
+const GlobalContext = createContext({})
 
 export const navLinks = [
   {
@@ -19,28 +19,41 @@ export const navLinks = [
     name: "Settings",
     path: "/settings",
   },
-];
+]
 
 export const linkIndexMap = {
-  'Home': 0,
-  'Apps': 1,
-  'Saves': 2,
-  'Settings': 3
+  Home: 0,
+  Apps: 1,
+  Saves: 2,
+  Settings: 3,
 }
+
 export function GlobalContextWrapper({ children }) {
-  const [currentPage, setCurrentPage] = useState();
-  let sharedState = {
+  const [currentPage, setCurrentPage] = useState()
+  const [appData, setAppData] = useState()
+  const [platformData, setPlatformData] = useState()
+  const [gameData, setGameData] = useState()
+  const [serverState, setServerState] = useState()
+
+  const sharedState = {
     currentPage,
-    setCurrentPage
+    setCurrentPage,
+    appData,
+    setAppData,
+    platformData,
+    setPlatformData,
+    gameData,
+    setGameData,
+    serverState,
   }
 
-  return (
-    <GlobalContext.Provider value={sharedState}>
-      {children}
-    </GlobalContext.Provider>
-  );
+  useEffect(() => {
+    console.log("Setting/Getting server state...")
+  }, [serverState, setServerState])
+
+  return <GlobalContext.Provider value={sharedState}>{children}</GlobalContext.Provider>
 }
 
 export function useGlobalContext() {
-  return useContext(GlobalContext);
+  return useContext(GlobalContext)
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,29 +2,35 @@ import { invoke } from "@tauri-apps/api/tauri"
 import type { NextPage } from "next"
 import Head from "next/head"
 import Image from "next/image"
-import { useEffect, useState } from "react";
+import { useEffect, useState } from "react"
 
+import { AppCarousel } from "@/components/AppCarousel"
+import { Container } from "@/components/Container"
 import { Navbar } from "@/components/Navbar"
-import { useGlobalContext } from "@/context/state";
-import { Container } from "@/components/Container";
-import { StorageCarousel } from "@/components/StorageCarousel";
-import { AppCarousel } from "@/components/AppCarousel";
-import { SaveCarousel } from "@/components/SaveCarousel";
+import { SaveCarousel } from "@/components/SaveCarousel"
+import { StorageCarousel } from "@/components/StorageCarousel"
+import { useGlobalContext } from "@/context/state"
 
 const Home: NextPage = () => {
-  const {currentPage, setCurrentPage} = useGlobalContext();
+  const { currentPage, setCurrentPage } = useGlobalContext()
 
   useEffect(() => {
-    setCurrentPage('Home')
-  }, [setCurrentPage]);
+    setCurrentPage("Home")
+  }, [setCurrentPage])
 
   return (
     <Container>
-      <div className='flex w-full'>
-        <ul className='flex flex-1 flex-col w-1/2 gap-20'>
-          <li><SaveCarousel /></li>
-          <li><AppCarousel /></li>
-          <li><StorageCarousel /></li>
+      <div className="flex w-full">
+        <ul className="flex w-1/2 flex-1 flex-col gap-20">
+          <li>
+            <SaveCarousel />
+          </li>
+          <li>
+            <AppCarousel />
+          </li>
+          <li>
+            <StorageCarousel />
+          </li>
         </ul>
       </div>
     </Container>
