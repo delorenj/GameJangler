fc5ef293758e48bf59883087ee907702e2849eb7
Jarad DeLorenzo <jdelorenzo@justworks.com>
2022-11-16 22:38:06 -0500

    Made some linting fixes and added ts types. Also, fixed some stuff with rust version and simplelog stuff

--- src/components/AppCarousel.tsx
+++ src/components/AppCarousel.tsx
@@ -1,7 +1,9 @@
+import React from "react"
+
 import { Card } from "@/components/Card"
 import { Carousel } from "@/components/Carousel"
 
-export const AppCarousel = () => {
+export const AppCarousel: React.FC = () => {
   return (
     <Carousel title="My Apps">
       <Card url="#" title="Test 1" description="This is a sample description" />
--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -1,16 +1,17 @@
-import { Card } from "@/components/Card";
+import React from "react"
+
+import { Card } from "@/components/Card"
 interface CarouselProps {
   title: string
   children: React.FC<typeof Card>[]
 }
 
-export const Carousel: React.FC<CarouselProps> = ({title, children}) => {
-
+export const Carousel: React.FC<CarouselProps> = ({ title, children }) => {
   return (
     <>
-      <h1 className='block text-2xl mb-4'>{title}</h1>
-      <ul className='flex flex-1 flex-row gap-12'>
-        {children.map(el => {
+      <h1 className="mb-4 block text-2xl">{title}</h1>
+      <ul className="flex flex-1 flex-row gap-12">
+        {children.map((el) => {
           return <li>{el}</li>
         })}
       </ul>
